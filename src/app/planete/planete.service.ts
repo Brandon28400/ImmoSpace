6fb900a4ae5523ba2c0f508771a76a8065c639b4
Brandon Rabour <brandonrabour@yahoo.fr>
2019-04-24 12:54:59 +0200

    fix bug

--- src/app/planete/planete.service.ts
+++ src/app/planete/planete.service.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { Injectable } from '@angular/core';
-import { Scene, Material } from 'three';
+// import { Scene, Material } from 'three';
 
 @Injectable({
   providedIn: 'root'
